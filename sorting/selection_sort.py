c218a85593cfb9e50b8b93285e53e2426a3e4815
Matthew Knop <knopmatthew93@gmail.com>
2020-12-21 01:46:30 -0700

    Removed comments.

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -9,16 +9,15 @@ class SelectionSort:
         self._lst = lst
     
     def sort(self, _lst):
-        for i in range(len(_lst) - 1, -1, -1):                         # O(n) - Interate over lst.
-            maxIndex = i                                                # O(1) - Set value.
-            for j in range(i - 1, -1, -1):                              # O(n) - Interate over lst.
-                print(_lst)                                            # O(1) - Print statement.
-                # Checking to see if element is the biggest.
-                if _lst[j] > _lst[maxIndex]:                          # O(1) - Compares indices.
-                    maxIndex = j                                        # O(1) - Set value.
+        for i in range(len(_lst) - 1, -1, -1): 
+            maxIndex = i 
+            for j in range(i - 1, -1, -1): 
+                print(_lst) 
 
-            # Swap and puts item into it's place
-            if maxIndex != i:                                           # O(1) - Check the max against the index.
-                _lst[maxIndex], _lst[i] = _lst[i], _lst[maxIndex]   # O(1) - Set values.
+                if _lst[j] > _lst[maxIndex]: 
+                    maxIndex = j
+
+            if maxIndex != i:
+                _lst[maxIndex], _lst[i] = _lst[i], _lst[maxIndex]
 
         return _lst
